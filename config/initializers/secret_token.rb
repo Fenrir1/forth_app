e0c4b23ae2e91f087036092127c746a83b7b6ca5
Fenrir <fenrir.new@gmail.com>
2012-04-16 13:35:07 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForthApp::Application.config.secret_token = '5e56d280a316aa879de96e384912cf7401e555fb1e17ef68fb8bff17c746271c5831b33a9a9dae668e7748994fc40e55b983544dcf88b874cd8e92c213cd885e'
